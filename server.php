4dbe635b446d423cb6d8ff3f4d400b9dc31a7adf
NayanaNarlapuram <50400338+NayanaNarlapuram@users.noreply.github.com>
2020-01-30 21:32:55 +1000

    server resolution

--- server.php
+++ server.php
@@ -65,7 +65,16 @@ if (isset($_POST['login_user'])) {
         array_push($errors, "Password is required");
     }
 
-   //paste
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            header('location: index.php');
+        }else {
+            array_push($errors, "Wrong username/password combination");
         }
     }
 }
