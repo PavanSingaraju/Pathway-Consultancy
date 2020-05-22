227bb51ced1e0b5df6bcdbdab34be190d6fa2b8f
ash279 <sweety1393@gmail.com>
2020-05-22 11:12:49 +0530

    Update EmployeeRegistration.php
    update 3

--- EmployeeRegistration.php
+++ EmployeeRegistration.php
@@ -33,4 +33,14 @@ if (isset($_POST['reg_employee'])) {
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
 
+    if ($user) { // if user exists
+        if ($user['username'] === $username) {
+            array_push($errors, "Username already exists");
+        }
+
+        if ($user['email'] === $email) {
+            array_push($errors, "email already exists");
+        }
+    }
+
     
