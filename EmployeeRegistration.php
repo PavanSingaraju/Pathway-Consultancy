ed6f927f1646c30bfe243f35d54b2f8096124d19
ash279 <sweety1393@gmail.com>
2020-05-22 11:01:05 +0530

    Update EmployeeRegistration.php
    update 2

--- EmployeeRegistration.php
+++ EmployeeRegistration.php
@@ -19,4 +19,18 @@ if (isset($_POST['reg_employee'])) {
     $class = mysqli_real_escape_string($db, $_POST['work_type']);
     $mobile = mysqli_real_escape_string($db, $_POST['mobile']);
 
+    // form validation: ensure that the form is correctly filled ...
+    // by adding (array_push()) corresponding error unto $errors array
+    if (empty($username)) { array_push($errors, "Username is required"); }
+    if (empty($email)) { array_push($errors, "Email is required"); }
+    if (empty($role)) { array_push($errors, "Work Role is required"); }
+    if (empty($class)) { array_push($errors, "Work type is required");}
+    if (empty($mobile)) { array_push($errors, "Contact No. is required");}
+
+    // first check the database to make sure
+    // a user does not already exist with the same username and/or email
+    $user_check_query = "SELECT * FROM Employee WHERE username='$username' OR email='$email' LIMIT 1";
+    $result = mysqli_query($db, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+
     
