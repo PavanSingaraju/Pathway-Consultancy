d89a720747d1bd2e4e4c18ff8e9f7b5ce0001dd1
PavanSingaraju <50400317+PavanSingaraju@users.noreply.github.com>
2020-05-22 09:12:08 +0530

    update login page footer

--- login.php
+++ login.php
@@ -49,24 +49,3 @@
 </nav>
 <div class="header">
     <h2>Sign In</h2>
-</div>
-
-<form method="post" action="login.php">
-    <?php include('errors.php'); ?>
-    <div class="input-group">
-        <label>Username</label>
-        <input type="text" name="username" >
-    </div>
-    <div class="input-group">
-        <label>Password</label>
-        <input type="password" name="password">
-    </div>
-    <div class="input-group">
-        <button type="submit" class="btn" name="login_user">Login</button>
-    </div>
-    <p>
-        Not yet a member? <a href="registration.php">Sign up</a>
-    </p>
-</form>
-</body>
-</html>
