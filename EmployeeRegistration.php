1c1399bb200e2e71c0d50717c666558eae0f0944
ash279 <sweety1393@gmail.com>
2020-05-22 10:28:23 +0530

    Update EmployeeRegistration.php
    update 1

--- EmployeeRegistration.php
+++ EmployeeRegistration.php
@@ -10,3 +10,13 @@ $errors = array();
 // connect to the database
 $db = mysqli_connect('localhost', 'ictatjcu_cons1', '123zxc', 'ictatjcu_cons1');
 //$db = mysqli_connect('localhost', 'root', '', 'pathwayconsultancy');
+// REGISTER USER
+if (isset($_POST['reg_employee'])) {
+    // receive all input values from the form
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $role = mysqli_real_escape_string($db, $_POST['role']);
+    $class = mysqli_real_escape_string($db, $_POST['work_type']);
+    $mobile = mysqli_real_escape_string($db, $_POST['mobile']);
+
+    
