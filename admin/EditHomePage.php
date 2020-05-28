119288d03b77fc080467749875cf81ec066efcb8
NayanaNarlapuram <50400338+NayanaNarlapuram@users.noreply.github.com>
2020-05-28 22:58:56 +1000

    home page modiied

---
+++ admin/EditHomePage.php
@@ -0,0 +1,17 @@
+<?php
+$db = mysqli_connect('localhost', 'ictatjcu_cons1', '123zxc', 'ictatjcu_cons1');
+$section = $_POST['Section'];
+$content = $_POST['Content'];
+if ($section == "welcome"){
+    mysqli_query($db,"UPDATE HomePageContent SET welcomeParagraph = '$content' WHERE id = 1");
+}elseif ($section == "services"){
+    mysqli_query($db,"UPDATE HomePageContent SET ServicesHeader = '$content' WHERE id = 1");
+}elseif ($section == "student"){
+    mysqli_query($db,"UPDATE HomePageContent SET StudentServicesParagraph = '$content' WHERE id = 1");
+}elseif ($section == "migration"){
+    mysqli_query($db,"UPDATE HomePageContent SET MigrationServicesParagraph = '$content' WHERE id = 1");
+}elseif ($section == "studyAus"){
+    mysqli_query($db,"UPDATE HomePageContent SET StudyInAustraliaParagraph = '$content' WHERE id = 1");
+}elseif ($section == "studyCan"){
+    mysqli_query($db,"UPDATE HomePageContent SET StudyInCanadaParagraph = '$content' WHERE id = 1");
+}
